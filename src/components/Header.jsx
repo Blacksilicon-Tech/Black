0ff8a5f5fd9e2c730d548af222937b769ec93866
Adelabu Segun Joshua <159425049+Shegzboyer12@users.noreply.github.com>
2025-07-20 23:18:18 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { assets } from "../asset/assets";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="flex border border-gray-500 rounded-full items-center justify-between w-11/12 h-16 lg:ml-14 mt-4">
       <div className="flex items-center ml-8 ">
-        <div className="flex items-center gap-2">
-          <img className="w-8" src={assets.black_logo} alt="" />
-          <h1 className="text-white text-2xl font-bold">Black Silicon</h1>
+        <div>
+          <Link to="/" className="flex gap-2"><img className="w-8" src={assets.black_logo} alt="" />
+          <h1 className="text-white text-2xl font-bold">Black Silicon</h1></Link>
         </div>
       </div>
       <Navbar />
